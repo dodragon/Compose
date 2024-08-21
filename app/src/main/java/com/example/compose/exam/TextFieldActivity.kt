6d8f2abe56d59ee47ce5200d62fce533b8ac9ba1
= <=>
2024-08-21 15:29:51 +0900

    Compose Learning - TextField - Scaffold - Snackbar

---
+++ app/src/main/java/com/example/compose/exam/TextFieldActivity.kt
@@ -0,0 +1,87 @@
+package com.example.compose.exam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose.base.BaseActivity
+import com.example.compose.ui.theme.ComposeTheme
+import kotlinx.coroutines.launch
+
+class TextFieldActivity : BaseActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ResultView()
+        }
+    }
+
+    @Composable
+    override fun ResultView() {
+        val (text, setValue) = remember {
+            mutableStateOf("")
+        }
+
+        val snackbarHostState = remember {
+            SnackbarHostState()
+        }
+        val scope = rememberCoroutineScope()
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+
+        Scaffold(
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = text,
+                    onValueChange = setValue
+                )
+                Button(onClick = {
+                    keyboardController?.hide()
+                    scope.launch {
+                        snackbarHostState.showSnackbar("result: $text")
+                    }
+                }) {
+                    Text(text = "Click")
+                }
+            }
+
+            it
+        }
+    }
+
+    override fun onBack() {
+        finish()
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun Preview() {
+        ComposeTheme {
+            ResultView()
+        }
+    }
+}
