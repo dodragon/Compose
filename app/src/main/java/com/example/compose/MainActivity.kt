751d1ea1feeb6307d08b3a6a109bef16144d2a5d
= <=>
2024-08-21 16:48:46 +0900

    Compose Learning - 메인 리스트 설정 - 비만도 계산기 준비

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -2,45 +2,52 @@ package com.example.compose
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Space
 import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose.base.BaseActivity
-import com.example.compose.exam.LazyColumnActivity
-import com.example.compose.exam.NavigationActivity
-import com.example.compose.exam.TextFieldActivity
-import com.example.compose.exam.ViewsActivity
-import com.example.compose.exam.VmExamActivity
+import com.example.compose.feature.ObesityCalculateActivity
 import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : BaseActivity() {
 
     private var backPressedTime: Long = 0
 
+    private val itemList = listOf(
+        "비만도 계산기",
+        "스톱워치",
+        "나만의 웹 브라우저",
+        "전자액자",
+        "수평 측정기",
+        "실로폰",
+        "GPS Map",
+        "Todo List"
+    )
+
+    private val activityList = listOf(
+        ObesityCalculateActivity::class.java,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, VmExamActivity::class.java))
 
         setContent {
             ComposeTheme {
                 Surface(
-                    //modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
                     ResultView()
@@ -51,29 +58,30 @@ class MainActivity : BaseActivity() {
 
     @Composable
     override fun ResultView() {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Blue)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            Text(text = "Hello What")
-            Text(text = "Hello Who")
+            items(itemList.size) { i ->
+                Button(
+                    onClick = {
+                        if(i < activityList.size) {
+                            startActivity(Intent(this@MainActivity, activityList[i]))
+                        }
+                    }
+                ) {
+                    Text(
+                        text = itemList[i],
+                        fontSize = 18.sp
+                    )
+                }
+            }
         }
     }
 
     override fun onBack() {
-        if(System.currentTimeMillis() - backPressedTime < 2000) {
-            backPressedTime = System.currentTimeMillis()
-            Toast.makeText(this, getString(R.string.main_back_toast), Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        if(System.currentTimeMillis() <= backPressedTime + 2000) {
-            finish()
-        }
+        finishAffinity()
     }
 
     @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/compose/StateActivity.kt
@@ -0,0 +1,68 @@
+package com.example.compose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.compose.base.BaseActivity
+import com.example.compose.ui.theme.ComposeTheme
+
+class StateActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ResultView()
+        }
+    }
+
+    @Composable
+    override fun ResultView() {
+        HomeScreen()
+    }
+
+    override fun onBack() {
+        finish()
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun Preview() {
+        ComposeTheme {
+            ResultView()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(
+    viewModel: StateViewModel = viewModel()
+) {
+    Column {
+        Text(text = "Hello World")
+        Button(onClick = {
+            viewModel.changeValue("와아아아아")
+        }) {
+            Text(text = "Click")
+        }
+    }
+}
+
+class StateViewModel : ViewModel() {
+    private val _value = mutableStateOf("Hello World")
+    val value: State<String> = _value
+
+    fun changeValue(
+        value: String
+    ) {
+        _value.value = value
+    }
+}
