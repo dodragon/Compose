fea7c3aa6791613a40872b509b5c444e51a264b7
= <=>
2024-08-22 10:57:46 +0900

    Compose Learning - 비만도 계산기 완료

--- app/src/main/java/com/example/compose/feature/ObesityCalculateActivity.kt
+++ app/src/main/java/com/example/compose/feature/ObesityCalculateActivity.kt
@@ -4,38 +4,220 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.R
 import com.example.compose.base.BaseActivity
 import com.example.compose.ui.theme.ComposeTheme
+import kotlin.math.pow
 
 class ObesityCalculateActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ResultView()
         }
     }
 
     @Composable
     override fun ResultView() {
-        TODO("Not yet implemented")
+        val viewModel = viewModel<BmiViewModel>()
+        val navController = rememberNavController()
+
+        val bmi = viewModel.bmi.value
+
+        NavHost(
+            navController = navController,
+            startDestination = "Home"
+        ) {
+            composable("Home") {
+                HomeScreen { height, weight ->
+                    viewModel.calculateBmi(height, weight)
+                    navController.navigate("Result")
+                }
+            }
+            composable("Result") {
+                ResultScreen(navController, bmi)
+            }
+        }
     }
 
     override fun onBack() {
-        TODO("Not yet implemented")
+        finish()
     }
 
     @Preview(showBackground = true)
     @Composable
     fun Preview() {
         ComposeTheme {
-            ResultView()
+            //ResultScreen(35.0)
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    onResultClicked: (Double, Double) -> Unit
+) {
+    val (height, setHeight) = rememberSaveable {
+        mutableStateOf("")
+    }
+    val (weight, setWeight) = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "비만도 계산기")
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(56.dp))
+            OutlinedTextField(
+                value = height,
+                onValueChange = setHeight,
+                label = { Text(text = "키") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            OutlinedTextField(
+                value = weight,
+                onValueChange = setWeight,
+                label = { Text(text = "몸무게") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = {
+                    if (height.isNotEmpty() && weight.isNotEmpty()) {
+                        onResultClicked(height.toDouble(), weight.toDouble())
+                    }
+                },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "결과")
+            }
+        }
+
+        it
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ResultScreen(
+    navController: NavController,
+    bmi: Double
+) {
+    val text = when {
+        bmi >= 35.0 -> "고도 비만"
+        bmi >= 30.0 -> "2단계 비만"
+        bmi >= 25.0 -> "1단계 비만"
+        bmi >= 23.0 -> "과체중"
+        bmi >= 18.5 -> "정상"
+        else -> "저체중"
+    }
+
+    val imageRes = when {
+        bmi >= 23.0 -> R.drawable.baseline_sentiment_very_dissatisfied_24
+        bmi >= 18.5 -> R.drawable.baseline_sentiment_satisfied_24
+        else -> R.drawable.baseline_sentiment_dissatisfied_24
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "비만도 계산기") },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Home",
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()
+                        }
+                    )
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(56.dp))
+            Text(text, fontSize = 30.sp)
+            Spacer(modifier = Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                colorFilter = ColorFilter.tint(color = Color.Black)
+            )
+        }
+
+        it
+    }
+}
+
+class BmiViewModel : ViewModel() {
+    private val _bmi = mutableDoubleStateOf(0.0)
+    val bmi: State<Double> = _bmi
+
+    fun calculateBmi(
+        height: Double,
+        weight: Double
+    ) {
+        _bmi.value = weight / (height / 100.0).pow(2.0)
     }
 }
